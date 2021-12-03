e58da96fb6d37d29969b029651f51caacd1dddce
salvable <49179440+salvable@users.noreply.github.com>
2021-12-03 20:10:14 +0900

    Add files via upload

---
+++ 프로그래머스/LV3/다단계 칫솔 판매.py
@@ -0,0 +1,33 @@
+def solution(enroll, referral, seller, amount):
+    
+    tree = {'-' : 'root'}
+    sell = {'-' : 0}
+    for i in range(len(enroll)):
+        child = enroll[i]
+        parent = referral[i]
+        sell[child] = 0
+        tree[child] = parent
+
+    for i in range(len(seller)):
+        child = seller[i]
+        parent = tree[child]
+        money = amount[i] * 100
+        sell[child] += money
+        while True:
+            if money == 0:
+                break
+            
+            commission = money // 10
+            sell[child] -= commission
+            sell[parent] += commission
+            child = parent
+            parent = tree[child]
+            money = commission
+            if parent == 'root':
+                break
+                
+    ans = []
+    for person in enroll:
+        ans.append(sell[person])
+    
+    return ans
