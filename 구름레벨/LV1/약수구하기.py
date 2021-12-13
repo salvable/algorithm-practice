3d686ecf25703424f3bb40e3854b4578f6ec72f3
salvable <49179440+salvable@users.noreply.github.com>
2021-12-13 18:49:28 +0900

    Delete 구름레벨/LV1 directory

--- 구름레벨/LV1/약수구하기.py
+++
@@ -1,5 +0,0 @@
-user_input = int(input())
-
-for i in range(1,user_input + 1):
-	if user_input % i == 0:
-		print(i, end=" ")
--- 구름레벨/LV1/홀짝판별.py
+++
@@ -1,6 +0,0 @@
-user_input = int(input())
-
-if user_input % 2 == 1:
-	print("odd")
-else:
-	print("even")
