6e694f737f1d3a8a903f23d58b4e57557edc42ee
salvable <salvable@kakao.com>
2021-12-17 17:34:44 +0900

    source tree 사용으로 인한 변경

---
+++ main.py
@@ -0,0 +1,13 @@
+from itertools import combinations, permutations
+from itertools import product
+from bisect import bisect_left, bisect_right
+from collections import Counter
+import heapq
+from collections import deque
+from sys import stdin
+import sys
+
+
+
+n = int(input())
+
