237e825d37f24103af6ffbd8c7bc56296decc737
salvable <salvable@kakao.com>
2021-12-20 18:30:04 +0900

    백준 유니온 파인드- 집합의 표현 풀이

--- 백준/유니온파인드/집합의 표현.py
+++ 백준/유니온파인드/집합의 표현.py
@@ -1,10 +1,3 @@
-from itertools import combinations, permutations
-from itertools import product
-from bisect import bisect_left, bisect_right
-from collections import Counter
-import heapq
-from collections import deque
-from sys import stdin
 import sys
 sys.setrecursionlimit(10**6)
 
