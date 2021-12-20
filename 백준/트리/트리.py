a76a35d70c612a0fb8992cfdc3ea433038a2bd32
salvable <salvable@kakao.com>
2021-12-20 17:36:09 +0900

    4803 트리 풀이

--- 백준/트리/트리.py
+++ 백준/트리/트리.py
@@ -1,11 +1,4 @@
-from itertools import combinations, permutations
-from itertools import product
-from bisect import bisect_left, bisect_right
-from collections import Counter
-import heapq
 from collections import deque
-from sys import stdin
-import sys
 
 case = 0
 
