e1d4796751d90111d0ac2e32f9717444a8738b15
salvable <salvable@kakao.com>
2022-01-06 21:50:59 +0900

    브루트포스 문제풀이

--- 백준/브루트포스/체스판칠하기.py
+++ 백준/브루트포스/체스판칠하기.py
@@ -1,13 +1,3 @@
-from itertools import combinations, permutations
-from itertools import product
-from bisect import bisect_left, bisect_right
-from collections import Counter
-import heapq
-from collections import deque
-from sys import stdin
-import sys
-import math
-
 n, m = map(int, input().split())
 chess = []
 change = []
