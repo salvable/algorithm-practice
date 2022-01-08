f570f3aa49b588ea6b1b51eb4ad2e3c40cf1edab
salvable <salvable@kakao.com>
2022-01-08 16:36:33 +0900

    no message

--- 백준/브루트포스/문자열.py
+++ 백준/브루트포스/문자열.py
@@ -1,13 +1,3 @@
-from itertools import combinations, permutations
-from itertools import product
-from bisect import bisect_left, bisect_right
-from collections import Counter
-import heapq
-from collections import deque
-from sys import stdin
-import sys
-import math
-
 a, b = input().split()
 
 answer = []
