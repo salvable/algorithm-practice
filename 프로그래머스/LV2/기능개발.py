9bc80ed765cf5c853c9e406a256f699ef946f0cc
salvable <salvable@kakao.com>
2022-01-24 15:58:05 +0900

    O(n^2) 이던 시간복잡도를 개선

--- 프로그래머스/LV2/기능개발.py
+++ 프로그래머스/LV2/기능개발.py
@@ -1,18 +1,22 @@
 def solution(progresses, speeds):
     answer = []
-    
+
+    time = 0
+    count = 0
+
     while progresses:
-        for i in range(len(progresses)):
-            progresses[i] += speeds[i]
-            
-        count = 0    
-            
-        while progresses and progresses[0] >= 100:
+        if progresses[0] + (time * speeds[0]) >= 100:
             count += 1
             progresses.pop(0)
             speeds.pop(0)
-        
-        if count > 0:
-            answer.append(count)
-    
+
+        else:
+            if count > 0:
+                answer.append(count)
+                count = 0
+
+            time += 1
+
+    # 마지막 작업은 무조건 100 이상으로 끝나므로 else문의 append가 적용되지 않으므로 while문을 탈출하고 나머지를 처리해줌
+    answer.append(count)
     return answer
