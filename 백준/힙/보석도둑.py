4106927dc56ee388064c92edb6816c8a68b49eda
salvable <salvable@kakao.com>
2022-05-23 20:16:09 +0900

    class 5 풀이

--- 백준/힙/보석도둑.py
+++ 백준/힙/보석도둑.py
@@ -1,20 +1,4 @@
-import copy
-import os
-from bisect import bisect_left, bisect_right
-from collections import Counter
 import heapq
-from collections import deque
-from sys import stdin
-import math
-from itertools import combinations, permutations, combinations_with_replacement, product
-import requests
-from collections import deque
-from copy import deepcopy
-import math
-
-dx = [0, 1, 0, -1]
-dy = [1, 0, -1, 0]
-
 import sys
 sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
