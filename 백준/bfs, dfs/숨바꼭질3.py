ce516d27a4f61fe68d62fdbb7306ed9c258b9e4b
salvable <salvable@kakao.com>
2022-06-03 20:19:35 +0900

    bfs 문제 풀이

--- 백준/bfs, dfs/숨바꼭질3.py
+++ 백준/bfs, dfs/숨바꼭질3.py
@@ -1,24 +1,8 @@
-import copy
-import os
-from bisect import bisect_left, bisect_right
-from collections import Counter
-import heapq
 from collections import deque
-from sys import stdin
-import math
-from itertools import combinations, permutations, combinations_with_replacement, product
-import requests
-from collections import deque
-from copy import deepcopy
-import math
-
-dx = [0, 1, 0, -1]
-dy = [1, 0, -1, 0]
 
 import sys
 sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
-
 
 n, k = map(int, input().split())
 q = deque()
